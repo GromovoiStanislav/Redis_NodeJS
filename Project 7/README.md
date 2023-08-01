f86f15a2cb04808a359946c83481b402f928722e
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-01 17:06:49 +0600

    Project 7

--- Project 7/README.md
+++ Project 7/README.md
@@ -1,1 +1,7 @@
 ## Redis cache simple example with Express (JS)
+
+```
+GET http://localhost:3000/photos
+GET http://localhost:3000/comments
+GET http://localhost:3000/todos
+```
--- Project 7/app.js
+++ Project 7/app.js
@@ -15,21 +15,63 @@ let redisClient;
   await redisClient.connect();
 })();
 
-const DEFAULT_EXPIRATION = 3600;
+const DEFAULT_EXPIRATION = 30;
 
 app.get('/photos', async (req, res) => {
   try {
     const cacheResults = await redisClient.get('photos');
 
     if (cacheResults) {
-      console.log('from cache');
+      console.log('photos from cache');
       return res.json(JSON.parse(cacheResults));
     } else {
       const { data } = await axios.get(
         'https://jsonplaceholder.typicode.com/photos'
       );
-      setCache('photos', data);
+      setCache('photos photos', data);
       console.log('from axios');
+      res.json(data);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404).send('Data unavailable');
+  }
+});
+
+app.get('/comments', async (req, res) => {
+  try {
+    const cacheResults = await redisClient.get('comments');
+
+    if (cacheResults) {
+      console.log('comments from cache');
+      return res.json(JSON.parse(cacheResults));
+    } else {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/comments'
+      );
+      setCache('comments', data);
+      console.log('comments from axios');
+      res.json(data);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404).send('Data unavailable');
+  }
+});
+
+app.get('/todos', async (req, res) => {
+  try {
+    const cacheResults = await redisClient.get('todos');
+
+    if (cacheResults) {
+      console.log('todos from cache');
+      return res.json(JSON.parse(cacheResults));
+    } else {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos'
+      );
+      setCache('todos', data);
+      console.log('todos from axios');
       res.json(data);
     }
   } catch (error) {
