1e8d247fec00353de5c26109254adfd59f90d7ef
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-21 16:45:57 +0600

    Project 25

--- Project 25/redis.js
+++ Project 25/redis.js
@@ -2,13 +2,6 @@ import 'dotenv/config';
 import { Redis } from '@upstash/redis';
 
 // Create Redis Client
-
-// export const redisClient = new Redis({
-//   url: 'https://worthy-glowworm-36772.upstash.io',
-//   token:
-//     'AY-kACQgZTJmZGYyNjAtMzFkMy00MmI2LTgxMTctZGE4YTg3Mjk5NWRiMzQyNzFjZmE5YTFhNDE2MDgyYjkyMDE2MTE4M2ZkZGE=',
-// });
-
 export const redisClient = Redis.fromEnv();
 
 console.log('Connected to Redis...');
